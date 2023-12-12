52fdc90a08881290900e7e4c4371472b5168e02c
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-12-12 00:45:05 -0800

    lab complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    badge_messages = []
+    for name in names:
+        badge_messages.append(f"Hello, my name is {name}.")
+    return badge_messages
+
 
 def assign_rooms(names):
-    return None
+    room_assignment = []
+    for index, name in enumerate(names):
+        room_assignment.append(
+            f"Hello, {name}! You'll be assigned to room {index + 1}!"
+        )
+    return room_assignment
+
 
 def printer(names):
-    return None
+    badge_messages = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    for message in badge_messages:
+        print(message)
+    for assignment in room_assignments:
+        print(assignment)
